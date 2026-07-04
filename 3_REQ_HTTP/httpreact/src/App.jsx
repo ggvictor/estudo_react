e0bc3e0c8fba5a6f6cb969176f8d257576c37de0
ggvictor <victorrodriguessilva2@gmail.com>
2026-07-03 21:07:55 -0300

    Jogo da velha

---
+++ 3_REQ_HTTP/httpreact/src/App.jsx
@@ -0,0 +1,77 @@
+import './App.css'
+
+import {useState, useEffect} from "react";
+
+// 4 - custom hook
+import { useFetch } from './hooks/useFetch';
+
+const url = "http://localhost:3000/products"
+function App() {
+  const [products, setProducts] = useState([])
+
+  // 4 - custom hook
+  const {data: items} = useFetch(url)
+
+  const [name, setName] = useState("")
+  const [price, setPrice] = useState("")
+
+// 1- resgatando dados
+  // useEffect(() =>{
+  //    const fetchData = async() =>{
+  //     const res = await fetch(url)
+
+  //     const data = await res.json()
+
+  //     setProducts(data)
+  //   }
+  //   fetchData()
+  // }, []);
+//  2 - adição de produtos
+  const handleSubmit = async(e) => {
+    e.preventDefault()
+
+    const product = {
+      name,
+      price
+    }
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type":"application/json"
+      },
+      body: JSON.stringify(product),
+    })
+  
+    // 3 - Carregamento dinâmico
+    const addedProduct = await res.json()
+     setProducts((prevProducts) =>[...prevProducts, addedProduct])
+    
+}
+
+  return (
+   <div className='App'>
+    <h1>Produtos</h1>
+    <ul>
+      {items && items.map((product) => (
+        <li key = {product.id}>{product.name} - R$:{product.price}</li>
+      ))}
+    </ul>
+    <div className="add-product">
+        <form onSubmit={handleSubmit}>
+            <label>
+              Nome:
+              <input type="text" value = {name} name='name' onChange={(e)=> setName(e.target.value)}/>
+            </label>
+
+            <label>
+              Preço:
+              <input type="number" value = {price} name='price' onChange={(e)=> setPrice(e.target.value)}/>
+            </label>
+            <input type="submit" value="Criar" />
+        </form>
+    </div>
+   </div>
+  )
+}
+
+export default App
